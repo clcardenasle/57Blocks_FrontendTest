7b1d1606b78cd7af763087cdd444895fc11d967b
Carol <carol_cardenas93@hotmail.com>
2021-10-26 15:12:17 -0500

    create layout component

---
+++ src/components/Layout/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Layout";
+
+const Layout = () => {
+  return (
+    <>
+        <Layout />
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = () => {
+
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+          <input></input>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
